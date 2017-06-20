d46c910c3abd92e340e97be974b427cf57f302bf
DenisLO <d.logvinov@belpol.ru>
2017-06-20 09:32:07 +0300

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -2,6 +2,9 @@ public class Calculate
 {
  public static void main(string[] arg ) {
 System.out.prinln(Calculate...);
-
+int first = Integer.valueOf (arg[0]);
+  int second = Integer.valueOf (arg[0]);
+  int summ = first + second;
+  System.out.println("Sum   " + sum);
 } 
 };

b099af73f328c95ec98127f51d2cc63aab8db378
DenisLO <d.logvinov@belpol.ru>
2017-06-20 09:43:45 +0300

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -2,8 +2,8 @@ public class Calculate
 {
  public static void main(string[] arg ) {
 System.out.prinln(Calculate...);
-int first = Integer.valueOf (arg[0]);
-  int second = Integer.valueOf (arg[1]);
+int first = Integer.valueOf(arg[0]);
+  int second = Integer.valueOf(arg[1]);
   int summ = first + second;
   System.out.println("Sum   " + sum);
 } 
